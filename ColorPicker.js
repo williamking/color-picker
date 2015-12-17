07fc6a40619305641e5c938303d59838d1dd35f8
williamking <williamjwking@gmail.com>
2015-12-18 00:16:25 +0800

    add the html

--- ColorPicker.js
+++ ColorPicker.js
@@ -3,20 +3,62 @@
     Description: A plug-in component for picking color
 */
 
-var ColorPicker = function() {
-    this.RGB_picker = function() {
+var ColorPicker =   {
+    RGB_picker: function() {
 	    this.pickerRG = document.createElement("canvas");
 	    this.pickerB = document.createElement("canvas");
 	    this.dom = document.createElement("div");
-        this.dom.className = "color-picker"
-        this.dom.append(this.pickerRG);
-        this.dom.append(this.pickerB);
+        this.dom.className = "color-picker";
+        this.pickerRG.className = "rg-picker";
+        this.pickerB.className = 'b-picker';
+        this.dom.appendChild(this.pickerRG);
+        this.dom.appendChild(this.pickerB);
+ 
+        this.RGCanvas = this.pickerRG.getContext('2d');
+        this.BCanvas = this.pickerB.getContext('2d');
+
+        this.PickRGEvent = (function(that) {
+            return function(e) {
+
+            }
+        })(this);
+
+        this.init();
     }   
 }
 
 ColorPicker.RGB_picker.prototype = {
 	addTo: function(element) {
-		element.append(this.dom);
+		element.appendChild(this.dom);
 	},
+    init: function(height, width) {
+        this.pickerRG.style.height = this.pickerB.style.height = (height || 40) + 'px';
+        this.pickerRG.style.width = this.pickerB.style.width = (width || 100) + 'px';
+        this.pickerRG.height = this.pickerB.height = ((height || 40));
+        this.pickerRG.width = this.pickerB.width = ((width || 100));
 
+        this.initCanvas();
+        this.addEvent();
+    },
+    addEvent: function() {
+        this.pickerRG.addEventListener(0, 0, this.pickerB.width, 0);
+
+    },
+    initCanvas: function() {
+        var gradient = this.RGCanvas.createLinearGradient(0, 0, this.pickerRG.width, 0);
+        gradient.addColorStop(0, '#F00');
+        gradient.addColorStop(0.2, '#FF0');
+        gradient.addColorStop(0.4, '#0F0');
+        gradient.addColorStop(0.6, '#0FF');
+        gradient.addColorStop(0.8, '#00F');
+        gradient.addColorStop(1, '#F0F');
+        this.RGCanvas.fillStyle = gradient;
+        this.RGCanvas.fillRect(0, 0, this.pickerRG.width, this.pickerRG.height);
+
+        gradient = this.BCanvas.createLinearGradient(0, 0, this.pickerB.width, 0);
+        gradient.addColorStop(0, '#FFFFFF');
+        gradient.addColorStop(1, '#FF0000');
+        this.BCanvas.fillStyle = gradient;
+        this.BCanvas.fillRect(0, 0, this.pickerB.width, this.pickerB.height);
+    }
 }
---
+++ test.js
@@ -0,0 +1,5 @@
+var panel = document.getElementById('color');
+
+var color_picker = new ColorPicker.RGB_picker();
+
+color_picker.addTo(panel);
